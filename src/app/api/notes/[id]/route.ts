416c97690a1f4b6ff131d9fe3da608fe3ccc95ca
omryakawar <omryakawar@gmail.com>
2025-12-25 14:36:22 +0530

    first update02

--- src/app/api/notes/[id]/route.ts
+++ src/app/api/notes/[id]/route.ts
@@ -1,15 +1,17 @@
-import {db} from '@/lib/db';
+import {getDB} from '@/lib/db';
 import {NextResponse} from 'next/server';
 
 export async function PUT(req:Request, {params}:any){
 const id = params.id;
 const body = await req.json();
+const db = await getDB();   
 const {title, content, tag} = body;
 await db.query('UPDATE notes SET title = ?, content = ?, tag = ? WHERE id = ?', [title, content, tag, id]);
 return NextResponse.json({message: 'Note updated successfully'});
 }
 export async function DELETE(req:Request, {params}:any){
     const id = params.id;
+    const db = await getDB();
     await db.query('DELETE FROM notes WHERE id = ?', [id]);
     return NextResponse.json({message: 'Note deleted successfully'});
 }
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,8 +1,15 @@
 import mysql from "mysql2/promise";
 
-export const db = await mysql.createConnection({
-  host: process.env.DB_HOST!,
-  user: process.env.DB_USER!,
-  password: process.env.DB_PASS!,
-  database: process.env.DB_NAME!,
-});
+let connection: any = null;
+
+export async function getDB() {
+  if (!connection) {
+    connection = await mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME
+    });
+  }
+  return connection;
+}
