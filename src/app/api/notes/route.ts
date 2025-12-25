ad084c5f4dd0d01610e5bb5ef1d61516c043ad81
omryakawar <omryakawar@gmail.com>
2025-12-25 13:54:57 +0530

    first update

--- src/app/api/notes/route.ts
+++ src/app/api/notes/route.ts
@@ -1,11 +1,14 @@
-import {db} from '@/lib/db'
+import { getDB } from "@/lib/db";
 import {NextResponse} from 'next/server'
 
 export async function GET() {
+    const db = await getDB();
     const [rows] = await db.query('SELECT * FROM notes ORDER BY created_at DESC');
     return NextResponse.json(rows);
 }
 export async function POST(req: Request){
+   
+    const db = await getDB();
     const body = await req.json();
     const {title, content, tag} = body;
     await db.query('INSERT INTO notes (title, content, tag) VALUES (?, ?, ?)', [title, content, tag]);
