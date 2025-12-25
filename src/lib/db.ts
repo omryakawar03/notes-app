e8945d0909823a471ae503b12d3a636001abde63
omryakawar <omryakawar@gmail.com>
2025-12-25 14:14:30 +0530

    first update02

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,15 +1,8 @@
 import mysql from "mysql2/promise";
 
-let conn: any = null;
-
-export async function getDB() {
-  if (!conn) {
-    conn = await mysql.createConnection({
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: process.env.DB_NAME,
-    });
-  }
-  return conn;
-}
+export const db = await mysql.createConnection({
+  host: process.env.DB_HOST!,
+  user: process.env.DB_USER!,
+  password: process.env.DB_PASS!,
+  database: process.env.DB_NAME!,
+});
