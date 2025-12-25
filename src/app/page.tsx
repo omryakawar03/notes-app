66104f98ea3ccd2a8578838ca9397c4c79335e8c
omryakawar <omryakawar@gmail.com>
2025-12-25 14:25:47 +0530

    first update02

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,31 @@
 import Link from 'next/link';
-export const dynamic = "force-dynamic";
 
 async function getNotes() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_APP_URL}/api/notes`, {
-    cache: "no-store",
-  });
-  return res.json();
+  try {
+    const res = await fetch("/api/notes", { cache: "no-store" });
+    if (!res.ok) throw new Error("Failed to fetch notes");
+    return res.json();
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
 }
 export default async function Home() {
     const notes = await getNotes();
   return (
-     <main className="p-6">
-      <Link className="bg-blue-500 text-white px-4 py-2 rounded" href="/notes/new">
-        + New Note
-      </Link>
-      <div className="mt-4 grid gap-3">
-        {notes.map((n: any) => (
+     <main className="p-6 flex flex-col justify-center items-center max-h-screen">
+      <div className="w-full max-w-2xl flex justify-end">
+        <Link className="bg-blue-500 text-white px-4 py-2 rounded" href="/notes/new">
+          + New Note
+        </Link>
+      </div>
+      <div className="mt-4 grid gap-3 bg-gray-400 p-4 rounded w-full max-w-2xl text-center">
+        {notes.length > 0 ? notes.map((n: any) => (
           <Link key={n.id} href={`/notes/${n.id}/edit`} className="border p-3 rounded">
             <h2 className="font-bold">{n.title}</h2>
             <p>{n.tags}</p>
           </Link>
-        ))}
+        )) : <p className="text-gray-500">No notes found</p>}
       </div>
     </main>
   );
